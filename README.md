f7479612ba9e5d8da0434477270b55f1155f3580
Ivan Greqov <96950329+greqov@users.noreply.github.com>
2022-04-15 10:13:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Template project
+
+## Features:
+
+- airbnb
+- eslint
+- prettier
+- webpack
+- tailwindcss
+
+## Development
+
+- build a project
+```bash
+npm run build
+```
+
+- start local dev server
+```bash
+npm run start
+```
+
+- publish 'dist' folder to github pages
+```bash
+npm run deploy
+```
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: './src/js/index.js',
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    watchFiles: ['./src/**/*.html'],
+    open: {
+      app: {
+        name: 'chromium',
+      },
+    },
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Template',
+      template: './src/index.html',
+      favicon: './src/favicon.ico',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
