1dffab41152a8726d37421cec10ae433fac85d1f
Ivan Greqov <idgreq@bk.ru>
2022-04-19 12:05:59 +0300

    UI: add markup (location, current weather, forecast)

--- src/js/UI.js
+++ src/js/UI.js
@@ -16,6 +16,8 @@ class UI {
     );
     document.body.insertAdjacentHTML('beforeend', layout);
     this.addHandlers();
+
+    // TODO: render default containers for location/weather?
   }
 
   addHandlers() {
@@ -36,14 +38,17 @@ class UI {
       const { lat, lon } = geoData[0];
       const weather = await API.getWeather(lat, lon);
       this.renderWeather(weather);
+
+      // TODO: refactor
+      this.renderForecast();
     });
   }
 
   renderLocation({ name, state, country }) {
-    // TODO: fix no state case (search 'pnk')
-    const tmplt = `location: ${name}, ${state}, ${country}<br/>`;
+    const stateStr = state ? `${state}, ` : '';
+    const template = `<h1 class="mt-10 text-2xl font-bold text-center">${name}, ${stateStr} ${country}</h1>`;
     const containerEl = document.querySelector('.js-main');
-    containerEl.insertAdjacentHTML('beforeend', tmplt);
+    containerEl.insertAdjacentHTML('beforeend', template);
   }
 
   renderWeather({ current }) {
@@ -51,20 +56,78 @@ class UI {
     const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
 
     const template = `
-      mememe<br/>
-      <img src="${iconUrl}" alt="${description}"/> ${main}<br/>
-      <div class="flex items-center">
-        wind:
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up inline-block" style="transform: rotate(${
-          current.wind_deg
-        }deg);"><line x1="12" y1="19" x2="12" y2="5"/><polyline points="5 12 12 5 19 12"/></svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-arrow-up-circle inline" style="transform: rotate(${
-          current.wind_deg
-        }deg);"><circle cx="12" cy="12" r="10"/><polyline points="16 12 12 8 8 12"/><line x1="12" y1="16" x2="12" y2="8"/></svg>
-        ${Math.round(current.wind_speed)}m/s
+    <div>
+      <img
+        class="block mx-auto"
+        src="${iconUrl}"
+        alt="${description}"
+      />
+      <span class="flex flex-wrap items-center justify-center italic space-x-2">
+        <span class="inline-block">${main},</span>
+        <span
+          class="inline-block border-b border-dotted cursor-default"
+          title="feels like ${Math.round(current.feels_like)}&deg;C"
+        >
+          ${Math.round(current.temp)}&deg;C,
+        </span>
+        <span class="inline-flex items-center">
+          <span class="pr-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="inline"
+              style="transform: rotate(${current.wind_deg}deg)"
+              data-darkreader-inline-stroke=""
+            >
+              <circle cx="12" cy="12" r="10"></circle>
+              <polyline points="16 12 12 8 8 12"></polyline>
+              <line x1="12" y1="16" x2="12" y2="8"></line>
+            </svg>
+          </span>
+          <span>${Math.round(current.wind_speed)}m/s</span>
+        </span>
+      </span>
+    </div>
+    `;
+
+    const containerEl = document.querySelector('.js-main');
+    containerEl.insertAdjacentHTML('beforeend', template);
+  }
+
+  renderForecast() {
+    const template = `
+    <div class="mx-auto my-10 border-b w-4/6"></div>
+
+    <div class="flex justify-center flex-wrap">
+      <div class="text-center">
+        <span class="block opacity-60">4/20</span>
+        <img
+          class="block mx-auto"
+          src="https://openweathermap.org/img/wn/04n@2x.png"
+          alt="overcast clouds"
+        />
+        <span class="block">2°C</span>
+        <span class="block">4m/s</span>
       </div>
-      temp: ${Math.round(current.temp)}<br/>
-      feels: ${Math.round(current.feels_like)}<br/>
+
+      <div class="text-center">
+        <span class="block opacity-60">4/21</span>
+        <img
+          class="block mx-auto"
+          src="https://openweathermap.org/img/wn/04n@2x.png"
+          alt="overcast clouds"
+        />
+        <span class="block">2°C</span>
+        <span class="block">4m/s</span>
+      </div>
+    </div>
     `;
 
     const containerEl = document.querySelector('.js-main');
