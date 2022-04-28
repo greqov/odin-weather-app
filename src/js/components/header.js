a1727d9544ef7742df7f346379eb5e22e558e326
Ivan Greqov <idgreq@bk.ru>
2022-04-28 10:53:00 +0300

    UI: set container max-width

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -9,7 +9,7 @@ if (!units) {
 function header() {
   return `
     <header class="mb-6 border-b">
-      <div class="container mx-auto py-6 px-4">
+      <div class="container max-w-5xl mx-auto py-6 px-4">
         <div class="flex items-center justify-between">
           <span class="block text-3xl font-bold">Weather App</span>
           <button
--- src/js/components/main.js
+++ src/js/components/main.js
@@ -1,7 +1,7 @@
 function main() {
   return `
-  <main class="js-main container mx-auto mb-6 px-4">
-    <form class="js-search-form" action="#" novalidate>
+  <main class="js-main container max-w-4xl mx-auto mb-6 px-4">
+    <form class="js-search-form mx-auto max-w-2xl" action="#" novalidate>
       <label class="block relative">
         <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-search absolute top-0 bottom-0 m-auto left-4 opacity-60"><circle cx="11" cy="11" r="8"/><line x1="21" y1="21" x2="16.65" y2="16.65"/></svg>
         <input
